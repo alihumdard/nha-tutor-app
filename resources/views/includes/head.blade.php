fac0cfdf08b18ae406de76535a52ddce9bd7f800
ali <coder.ali.381.com>
2025-07-11 04:10:35 -0700

    home page responsive

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,2 +1,5 @@
 <!-- Font Awesome for icons -->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet" />
+<!-- Add this in your <head> section -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
